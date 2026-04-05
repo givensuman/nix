2b9d6ced7c79f39ef5f2b60248fa5828f69250fc
givensuman <givensuman@duck.com>
2026-04-05 14:27:58 -0400

    Update flatpaks

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -4,15 +4,46 @@
   ];
 
   services.flatpak.enable = true;
-  services.flatpak.remotes = [{
-    name = "flathub";
-    location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-  }];
+  services.flatpak.remotes = [
+    {
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }
+    {
+      name = "gnome-nightly";
+      location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo";
+    }
+  ];
   services.flatpak.packages = [
     "org.mozilla.firefox"
     "com.spotify.Client"
-    "com.discordapp.Discord"
     "io.podman_desktop.PodmanDesktop"
+    "io.github.milkshiift.GoofCord"
+    "org.localsend.localsend_app"
+    "de.haeckerfelix.Fragments"
+    # Flatpak utilities
+    "com.github.tchx84.Flatseal"
+    "io.github.flattool.Warehouse"
+    "io.github.flattool.Ignition"
+    # GTK/GNOME desktop applications
+    "org.gnome.baobab"    # Disk Usage Analyzer
+    "org.gnome.Boxes"
+    "org.gnome.Calendar"
+    "org.gnome.Characters"
+    "org.gnome.clocks"
+    "org.gnome.Contacts"
+    "org.gnome.Firmware"
+    "org.gnome.font-viewer"
+    "org.gnome.Logs"
+    "org.gnome.Loupe"     # Image Viewer
+    "org.gnome.Maps"
+    "org.gnome.Papers"    # Document Viewer
+    "org.gnome.Showtime"  # Video Player
+    "org.gnome.Snapshot"  # Camera
+    "org.gnome.TextEditor"
+    "org.gnome.Weather"
+    "org.gtk.Gtk3theme.adw-gtk3"
+    "org.gtk.Gtk3theme.adw-gtk3-dark"
   ];
   services.flatpak.overrides = {
     global = {
@@ -48,7 +79,9 @@
   services.desktopManager.cosmic.enable = true;
   services.system76-scheduler.enable = true;
   environment.cosmic.excludePackages = with pkgs; [
-    cosmic-edit   # Prefer Neovim
+    cosmic-edit   # Prefer GTK
+    cosmic-player
+    cosmic-reader
     cosmic-store  # Prefer Bazaar
     cosmic-term   # Prefer Ghostty
   ];
